84f6168eaaaa3a5240b42ffa3e49b3ec57024661
AleksaDeric123 <aleksa.djeric@gmail.com>
2015-02-24 10:47:42 -0500

    change your changes

--- src/moneyMarket.java
+++ src/moneyMarket.java
@@ -1,10 +1,29 @@
 public class moneyMarket extends commonBank {
 
+	
+	final int maxW = 6;
+	int timesWdrwn = 0;
 	public moneyMarket() {
-
+		System.out.println("A Money Market Account Was Created\n");
+		assignCustomerID();
 	}
+	public moneyMarket(String firstName, String lastName, int creditScore,
+			String phoneNumber, String SocialSecurity, String adress) {
 
-	public void issureCard() {
+		System.out.println("A Money Market Account Was Created\n");
+		assignCustomerID();
+		assignAgent();
+		setSocialSecurity(SocialSecurity);
+		tempCustomer.setFirstName(firstName);
+		tempCustomer.setLastName(lastName);
+		tempCustomer.setPhoneNumber(phoneNumber);
+		tempCustomer.setAddress(adress);
+		tempCustomer.setCreditScore(creditScore);
+		tempCustomer.setBalance(0.0);
+	}
+	
+	
+	public void issueCard() {
 		// TODO Auto-generated method stub
 
 	}
@@ -15,7 +34,7 @@ public class moneyMarket extends commonBank {
 	}
 
 	public void getWithdrawalPolicy() {
-		// TODO Auto-generated method stub
+		System.out.println("");
 
 	}
 
@@ -24,22 +43,53 @@ public class moneyMarket extends commonBank {
 
 	}
 
-	public void withdraw(int amount) {
-		// TODO Auto-generated method stub
-
+	public void withdraw(Double amount) {
+		if (maxW > timesWdrwn)
+		{
+		tempCustomer.setBalance((tempCustomer.getBalance() - amount));
+		timesWdrwn++;
+		System.out.println("You can withdraw your money" + (6-timesWdrwn) + "more time(s).");
+		}
+		else
+		{
+		System.out.println("You can no longer withdraw your money.");	
+		}
+		
 	}
 
-	@Override
+	
 	public void getAddress() {
 		// TODO Auto-generated method stub
 
 	}
 
 	public void assignCustomerID() {
-
+		
 		int rand = (int) (Math.random() * 1000000) + 32343;
 		tempCustomer.setCustomerID("MM-" + rand);
+		timesWdrwn++;
+		
+	}
+	
+	public String toString() {
+		return tempCustomer.toString();
 
+	}
+
+	@Override
+	public void issureCard() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public Double getFutureBalance(int months) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void deposit(Double amount) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
